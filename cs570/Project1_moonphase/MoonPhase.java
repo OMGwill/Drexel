7f59e3f525c463d70beb981cbdec430d905bbc15
OMGwill <wluttmann@live.esu.edu>
2020-05-16 17:47:56 -0400

    added zodiac symbols

--- cs570/Project1_moonphase/MoonPhase.java
+++ cs570/Project1_moonphase/MoonPhase.java
@@ -16,22 +16,21 @@ public class MoonPhase{
 	//************GLOBAL*************
 	private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-	//seed for know new moon date/time from https://www.space.com/18880-moon-phases.html
-	//private static final LocalDateTime dateSeed = LocalDateTime.parse("2020-01-24 16:42:00", dateFormat);
-
 	//seed for know new moon date/time from /https://www.almanac.com/astronomy/moon/calendar/zipcode/19382/2020-01
 	private static final LocalDateTime dateSeed = LocalDateTime.parse("2020-01-24 16:44:00", dateFormat);
 
 
+	//from https://www.moontracks.com/lunar_ingress.html
+	private static final LocalDateTime moonSignSeed = LocalDateTime.parse("2020-04-02 18:26:00", dateFormat);
+
+
 	//from https://minkukel.com/en/various/calculating-moon-phase/
-	private static final double lunarCycle = 29.53058770576;
+	private static final double lunarCycle = 29.53058770576; //days
 
 
 	//from https://en.wikipedia.org/wiki/Moon
-	private static final double moonRevolution = 655.719864; //27.321661 days * 24h
+	private static final double moonRevolution = 655.719864; //27.321661 days * 24h = 655.719864 hours for 1 full orbit revolution
 
-	//from https://www.moontracks.com/lunar_ingress.html
-	private static final LocalDateTime moonSignSeed = LocalDateTime.parse("2020-04-02 18:26:00", dateFormat);
 
 	//all possible phases of moon
 	public static enum PHASE{
@@ -63,7 +62,7 @@ public class MoonPhase{
 		System.out.println("* Hello! Today is " + formatDate(d) + "\t\t\t*");
 		System.out.printf("* The moon age since last New Moon is: %.2f days\t*\n",age);
 		System.out.println("* The moon's phase is: " + getMoonPhase(age)+ "\t\t\t*");
-		System.out.println("* The sun is in: " + getSunSign(d)+ "\t\t\t\t\t*");
+		System.out.println("* The sun is in: " + getSunSign(d)+ "\t\t\t\t*");
 		System.out.println("* The moon is in: " + getMoonSign(d)+ "\t\t\t\t*");
 		System.out.println("*********************************************************");
 	}
@@ -164,7 +163,7 @@ public class MoonPhase{
 	*/
 	public static double getMoonAge (LocalDateTime d){
 		Duration dateDiff = Duration.between(dateSeed,d);
-		double daysDiff = dateDiff.toHours()/24.0;
+		double daysDiff = dateDiff.toHours()/24.0; //gets day difference with double precision -- Duration.toDays() returns a long
 
 		if(daysDiff >=0)
 			return (daysDiff % lunarCycle);
@@ -182,92 +181,97 @@ public class MoonPhase{
 	public static String getMoonPhase(double moonAge){
 		int ageInDays = (int)Math.round(moonAge);
 		switch(ageInDays){
-			case 0: return PHASE.NEW_MOON.toString();
-			case 1: 
-			case 2: 
-			case 3: 
-			case 4:  
-			case 5: 
-			case 6: return PHASE.WAXING_CRESCENT.toString();
-			case 7: return PHASE.FIRST_QUARTER.toString();
-			case 8: 
-			case 9: 
-			case 10: 
-			case 11: 
-			case 12: 
-			case 13: 
-			case 14: return PHASE.WAXING_GIBBOUS.toString();
-			case 15: return PHASE.FULL_MOON.toString();
-			case 16:  
-			case 17: 
-			case 18: 
-			case 19: 
-			case 20:
-			case 21: return PHASE.WANING_GIBBOUS.toString();
-			case 22: return PHASE.THIRD_QUARTER.toString();
-			case 23:  
-			case 24: 
-			case 25: 
-			case 26: 
-			case 27: 
-			case 28: 
-			case 29: return PHASE.WANING_CRESCENT.toString();
-			default: return PHASE.UNKOWN.toString();
-		} 
+				case 0: return PHASE.NEW_MOON.toString().toLowerCase().replace("_"," ");
+				case 1: 
+				case 2: 
+				case 3: 
+				case 4:  
+				case 5: 
+				case 6: return PHASE.WAXING_CRESCENT.toString().toLowerCase().replace("_"," ");
+				case 7: return PHASE.FIRST_QUARTER.toString().toLowerCase().replace("_"," ");
+				case 8: 
+				case 9: 
+				case 10: 
+				case 11: 
+				case 12: 
+				case 13: 
+				case 14: return PHASE.WAXING_GIBBOUS.toString().toLowerCase().replace("_"," ");
+				case 15: return PHASE.FULL_MOON.toString().toLowerCase().replace("_"," ");
+				case 16:  
+				case 17: 
+				case 18: 
+				case 19: 
+				case 20:
+				case 21: return PHASE.WANING_GIBBOUS.toString().toLowerCase().replace("_"," ");
+				case 22: return PHASE.THIRD_QUARTER.toString().toLowerCase().replace("_"," ");
+				case 23:  
+				case 24: 
+				case 25: 
+				case 26: 
+				case 27: 
+				case 28: 
+				case 29: return PHASE.WANING_CRESCENT.toString().toLowerCase().replace("_"," ");
+				default: return PHASE.UNKOWN.toString().toLowerCase().replace("_"," ");
+			} 
 
-	}
+		}
 
 
-	public static String getSunSign(LocalDateTime d){
+		public static String getSunSign(LocalDateTime d){
 
-		DateTimeFormatter monthDay = DateTimeFormatter.ofPattern("MM-dd");
-		String x = d.format(monthDay);
-		String sign = "-1";
+			DateTimeFormatter monthDay = DateTimeFormatter.ofPattern("MM-dd");
+			String x = d.format(monthDay);
+			String sign = "-1";
 
 		if(x.compareTo("03-21")>=0 && x.compareTo("04-19")<=0){
-			sign = "Aries";
+			sign = "Aries ♈";
 		}
 		if(x.compareTo("04-20")>=0 && x.compareTo("05-20")<=0){
-			sign = "Taurus";
+			sign = "Taurus ♉";
 		}
 		if(x.compareTo("05-21")>=0 && x.compareTo("06-21")<=0){
-			sign = "Gemini";
+			sign = "Gemini ♊";
 		}
 		if(x.compareTo("06-22")>=0 && x.compareTo("07-22")<=0){
-			sign = "Cancer";
+			sign = "Cancer ♋";
 		}
 		if(x.compareTo("07-23")>=0 && x.compareTo("08-22")<=0){
-			sign = "Leo";
+			sign = "Leo ♌";
 		}
 		if(x.compareTo("08-23")>=0 && x.compareTo("09-22")<=0){
-			sign = "Virgo";
+			sign = "Virgo ♍";
 		}
 		if(x.compareTo("09-23")>=0 && x.compareTo("10-23")<=0){
-			sign = "Libra";
+			sign = "Libra ♎";
 		}
 		if(x.compareTo("10-24")>=0 && x.compareTo("11-22")<=0){
-			sign = "Scorpio";
+			sign = "Scorpio ♏";
 		}
 		if(x.compareTo("11-23")>=0 && x.compareTo("12-21")<=0){
-			sign = "Sagittarius";
+			sign = "Sagittarius ♐";
 		}
 		if(x.compareTo("12-22")>=0 && x.compareTo("12-31")<=0){
-			sign = "Capricorn";
+			sign = "Capricorn ♑";
 		}
 		if(x.compareTo("01-01")>=0 && x.compareTo("01-19")<=0){
-			sign = "Capricorn";
+			sign = "Capricorn ♑";
 		}
 		if(x.compareTo("01-20")>=0 && x.compareTo("02-18")<=0){
-			sign = "Aquarius";
+			sign = "Aquarius ♒";
 		}
 		if(x.compareTo("02-19")>=0 && x.compareTo("03-20")<=0){
-			sign = "Pisces";
+			sign = "Pisces ♓";
 		}
 
 
 		return sign;
 	}
 
+
+	//1 full moon revolution is 655.719864 hours
+	//to find sign, find difference in hours from target date and % 655.719864
+	//dividing 655.719864 by 12 signs, results in the moon being in each sign for 54.643322 hours
+	//i used https://www.astrocal.co.uk/moon-sign-calculator/ to check for timestamp correctness
 	public static String getMoonSign(LocalDateTime d){
 		Duration dateDiff = Duration.between(moonSignSeed,d);
 		double hourDiff = dateDiff.toHours();
@@ -280,31 +284,30 @@ public class MoonPhase{
 			position = (moonRevolution + (hourDiff % moonRevolution));
 
 
-
 		if(position >= 0 && position <= 54.643322)
-			sign = "Leo";
+			sign = "Leo ♌";
 		if(position >= 54.643323 && position <= 109.286644)
-			sign = "Virgo";
+			sign = "Virgo ♍";
 		if(position >= 109.28665 && position <= 163.929966)
-			sign = "Libra";
+			sign = "Libra ♎";
 		if(position >= 163.929967 && position <= 218.573288)
-			sign = "Scorpio";
+			sign = "Scorpio ♏";
 		if(position >= 218.573289 && position <= 273.21661)
-			sign = "Sagittarius";
+			sign = "Sagittarius ♐";
 		if(position >= 273.21662 && position <= 327.859932)
-			sign = "Capricorn";
+			sign = "Capricorn ♑";
 		if(position >= 327.859933 && position <= 382.503254)
-			sign = "Aquarius";
+			sign = "Aquarius ♒";
 		if(position >= 382.503255 && position <= 437.146576)
-			sign = "Pisces";
+			sign = "Pisces ♓";
 		if(position >= 437.146577 && position <= 491.789898)
-			sign = "Aries";
+			sign = "Aries ♈";
 		if(position >= 491.789899 && position <= 546.43322)
-			sign = "Taurus";
+			sign = "Taurus ♉";
 		if(position >= 546.43323 && position <= 601.076542)
-			sign = "Gemini";
+			sign = "Gemini ♊";
 		if(position >= 601.076543 && position <= 655.719864)
-			sign = "Cancer";
+			sign = "Cancer ♋";
 
 
 
@@ -321,12 +324,9 @@ public class MoonPhase{
 
 	//start main
 	public static void main(String[] args){
-		//create date/time object from right now -- testing specific times
+		
 		Scanner scnr = new Scanner(System.in);
 		LocalDateTime currDate = LocalDateTime.now();
-		// LocalDateTime testDateFuture = LocalDateTime.parse("2020-05-04 15:04:00",dateFormat);
-		// LocalDateTime testDatePast = LocalDateTime.parse("2018-05-04 15:04:00",dateFormat);
-		 LocalDateTime myBirthday = LocalDateTime.parse("1990-11-05 06:28:00",dateFormat);
 		double theMoonAge;
 		String theMoonPhase;
 		String continueFlag = "y";
@@ -341,67 +341,6 @@ public class MoonPhase{
 
 		System.out.println();
 		System.out.println();
-
-
-		// //test compute moon age -- given test dates
-		// System.out.println("<<<<<<<<<<<<<<<<<<<<<<<TEST CASES>>>>>>>>>>>>>>>>>>>>");
-
-		// theMoonAge=getMoonAge(currDate);
-		// theMoonPhase=getMoonPhase(theMoonAge);
-		// System.out.println("Current time is : "+formatDate(currDate));
-		// System.out.println("current moon age: " + theMoonAge + "\nPhase: " + theMoonPhase);
-		// System.out.println();
-
-		// theMoonAge=getMoonAge(currDate.plusHours(5));
-		// theMoonPhase=getMoonPhase(theMoonAge);
-		// System.out.println("Current time is : "+formatDate(currDate.plusHours(5)));	
-		// System.out.println("current moon age: " + theMoonAge + "\nPhase: " + theMoonPhase);
-		// System.out.println();
-
-		// theMoonAge=getMoonAge(testDateFuture);
-		// theMoonPhase=getMoonPhase(theMoonAge);
-		// System.out.println("Current time is : "+formatDate(testDateFuture));
-		// System.out.println("May 4 2020 age: " + theMoonAge + "\nPhase: " + theMoonPhase);
-		// System.out.println();
-
-		// theMoonAge=getMoonAge(testDatePast);
-		// theMoonPhase=getMoonPhase(theMoonAge);
-		// System.out.println("Current time is : "+formatDate(testDatePast));
-		// System.out.println("May 4 2018 age: " + theMoonAge + "\nPhase: " + theMoonPhase);
-		// System.out.println();
-		
-		// theMoonAge=getMoonAge(myBirthday);
-		// theMoonPhase=getMoonPhase(theMoonAge);
-		// System.out.println("Current time is : "+formatDate(myBirthday));
-		// System.out.println("my birthday age: " + theMoonAge + "\nPhase: " + theMoonPhase);
-		// System.out.println();
-
-		// //test lunar calendar from current time
-		// System.out.println();
-		// System.out.println();
-
-		// System.out.println("<<<<<<<<<<Testing full lunar calendar>>>>>>>>>>>>>");
-		// for(int i=0;i<32;i++){
-		// 	currDate =LocalDateTime.now().plusDays(i);
-		// 	theMoonAge=getMoonAge(currDate);
-		// 	theMoonPhase=getMoonPhase(theMoonAge);
-
-		// 	System.out.println("Date: " + formatDate(currDate));
-		// 	System.out.println("Moon age: " + theMoonAge);
-		// 	System.out.println("Moon Phase: " + theMoonPhase);
-		// 	System.out.println();
-
-
-		// }
-
-		// System.out.println("<<<<<<<<End testing full lunar calendar>>>>>>>>>>>");
-
-		// //end compute moon age -- given test dates
-		// System.out.println("<<<<<<<<<<<<<<<<<<<<<END TEST CASES>>>>>>>>>>>>>>>>>>");
-		// System.out.println();
-		// System.out.println();
-
-
 
 		//print welcome message
 		currDate = LocalDateTime.now();
@@ -460,37 +399,8 @@ public class MoonPhase{
 
 		} while (!continueFlag.equalsIgnoreCase("N"));
 
-
-
-
-
-		// System.out.println(sunSign);
-		// System.out.println(getSunSign(myBirthday));
-		
-		// for(int i=0; i < 12;i++){
-		// 	System.out.print(currDate + " ");
-		// 	sunSign = getSunSign(currDate);
-		// 	System.out.println(sunSign);
-		// 	currDate = currDate.plusMonths(1);
-		// }
-
-
-		// String sunSign = getSunSign(currDate);
-
-		// System.out.println(sunSign);
-		// System.out.println(getSunSign(myBirthday));
-		
-		// for(int i=0; i < 12;i++){
-		// 	System.out.print(currDate + " ");
-		// 	sunSign = getSunSign(currDate);
-		// 	System.out.println(sunSign);
-		// 	currDate = currDate.plusMonths(1);
-		// }
-
 		//todo:
 		//print ascii art for each phase
-		//return sun (zodiac) sign - param(given date)
-		//**if theres time** add method to calculate moon sign - param (given date and time) - research required
 
 	}//end main
 }//end class
